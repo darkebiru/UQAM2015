6ad039580f29becbfc6012e8de6d0b5ea213c794
PierreC2 <pierrecaron2@netscape.net>
2016-01-11 21:15:13 -0500

    Create Valeurfonciere.java
    Classe principale

---
+++ Valeurfonciere.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valeurfonciere;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class ValeurFonciere {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Commentaire
+        // TODO code application logic here
+    }
+    
+}
